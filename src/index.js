24d96de94095a29ed24b37dc9a017e0f19c9acaf
Marcelo Freitas <markfreitaas@outlook.com>
2024-07-29 20:47:05 -0300

    alteração da rota login

--- src/index.js
+++ src/index.js
@@ -1,19 +1,23 @@
 const express = require("express");
+const cors = require("cors")
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
 const app = express();
+//const { v4: uuidv4 } = require('uuid');
+
 const port = 3000;
+
+app.use(cors())
+//uuidv4();
+
+
 
 app.use(bodyParser.json());
 
-
 const users = [];
-const messages = [];
-
-
+const messages = []
 const isEmailRegistered = (email) => users.some((user) => user.email === email);
 const getUserByEmail = (email) => users.find((user) => user.email === email);
-
 
 // Criar um novo usuário
 app.post("/signup", (request, response) => {
@@ -50,7 +54,7 @@ app.post("/signup", (request, response) => {
 
     // Criação do novo usuário com ID gerado automaticamente
     const newUser = {
-      id: users.length + 1, 
+      id: users.length + 1,
       name,
       email,
       password: hashedPassword,
@@ -58,23 +62,55 @@ app.post("/signup", (request, response) => {
 
     users.push(newUser);
 
-    return response
-      .status(201)
-      .json({
-        message: `Bem-Vindo ${name}! Pessoa usuária registrada com sucesso.`,
-        user: newUser,
-      });
+    return response.status(201).json({
+      message: `Bem-Vindo ${name}! Pessoa usuária registrada com sucesso.`,
+      user: newUser,
+    });
   });
 });
 // Entrar em uma conta existente
-app.post("/login", (request, response) => {
+app.post("/login", async (request, response) => {
   const { email, password } = request.body;
+
+  const user = users.find(user => user.email === email)
+  
+  if (!user) {
+    return response.status(404).json({message: "Usuário não encontrado.",});
+  }
+
+  const passwordMatch = await bcrypt.compare(password, user.password)
+
+  if (!passwordMatch) {
+    return response.status(404).json({ message: "Credenciais Invalidas." });
+  }
+  
+  return response.status(200).json({
+      message: `Seja bem-vindo ${user.name}!`,
+      user: user.email
+    });
+  });
+
+
+// Endpoint para enviar uma nova mensagem
+app.post("/message", (request, response) => {
+  const { email, title, description } = request.body;
 
   if (!email) {
     return response.status(400).json({ message: "Insira um e-mail válido." });
   }
-  if (!password) {
-    return response.status(400).json({ message: "Insira uma senha válida." });
+  if (!title) {
+    return response
+      .status(400)
+      .json({
+        message: "Por favor, verifique se passou o título da mensagem.",
+      });
+  }
+  if (!description) {
+    return response
+      .status(400)
+      .json({
+        message: "Por favor, verifique se passou a descrição da mensagem.",
+      });
   }
 
   const user = getUserByEmail(email);
@@ -87,105 +123,126 @@ app.post("/login", (request, response) => {
       });
   }
 
-  bcrypt.compare(password, user.password, (err, isMatch) => {
-    if (err) {
-      return response
-        .status(500)
-        .json({ message: "Erro ao verificar a senha." });
-    }
-    if (!isMatch) {
-      return response.status(400).json({ message: "Senha incorreta." });
-    }
+  // Criação da nova mensagem com ID gerado automaticamente
+  const newMessage = {
+    id: messages.length + 1,
+    title,
+    description,
+    userId: user.id, // Relaciona a mensagem ao usuário
+  };
 
-    return response
-      .status(200)
-      .json({
-        message: `Seja bem-vindo ${user.name}! Pessoa usuária logada com sucesso!`,
-      });
-  });
-});
-// Endpoint para enviar uma nova mensagem
-app.post('/message', (request, response) => {
-    const { email, title, description } = request.body;
 
-    if (!email) {
-        return response.status(400).json({ message: 'Insira um e-mail válido.' });
-    }
-    if (!title) {
-        return response.status(400).json({ message: 'Por favor, verifique se passou o título da mensagem.' });
-    }
-    if (!description) {
-        return response.status(400).json({ message: 'Por favor, verifique se passou a descrição da mensagem.' });
-    }
+  messages.push(newMessage);
 
-    const user = getUserByEmail(email);
-    if (!user) {
-        return response.status(404).json({ message: 'Email não encontrado no sistema, verifique ou crie uma conta.' });
-    }
-
-    // Criação da nova mensagem com ID gerado automaticamente
-    const newMessage = {
-        id: messages.length + 1, 
-        title,
-        description,
-        userId: user.id, // Relaciona a mensagem ao usuário
-    };
-
-    messages.push(newMessage);
-
-    return response.status(201).json({ message: `Mensagem criada com sucesso!`, messageData: newMessage.description });
+  return response
+    .status(201)
+    .json({
+      message: `Mensagem criada com sucesso!`,
+      messageData: newMessage
+    });
 });
 // Endpoint para listar as mensagens
-app.get('/message/:email', (request, response) => {
-    const { email } = request.params;
+app.get("/message/:email", (request, response) => {
+  const { email } = request.params;
 
-    if (!isEmailRegistered(email)) {
-        return response.status(404).json({ message: 'Email não encontrado no sistema, verifique ou crie uma conta.' });
-    }
+  if (!isEmailRegistered(email)) {
+    return response
+      .status(404)
+      .json({
+        message:
+          "Email não encontrado no sistema, verifique ou crie uma conta.",
+      });
+  }
 
-    const userMessages = messages.filter(message => {
-        const user = getUserByEmail(email);
-        return message.userId === user.id;
+  const userMessages = messages.filter((message) => {
+    const user = getUserByEmail(email);
+    return message.userId === user.id;
+  });
+
+  return response
+    .status(200)
+    .json({
+      message: `Seja bem-vindo! Aqui estão as suas mensagens:`,
+      messages: userMessages,
     });
-
-    return response.status(200).json({ message: `Seja bem-vindo! Aqui estão as suas mensagens:`, messages: userMessages});
 });
 // Endpoint para atualizar as mensagens
-app.put('/message/:id', (request, response) => {
-    const { id } = request.params;
-    const { title, description } = request.body;
+app.put("/message/:id", (request, response) => {
+  const { id } = request.params;
+  const { title, description } = request.body;
 
-    const message = messages.find(msg => msg.id === parseInt(id));
+  const message = messages.find((msg) => msg.id == parseInt(id));
 
-    if (!message) {
-        return response.status(404).json({ message: 'Por favor, informe um id válido da mensagem' });
-    }
-    if (!title) {
-        return response.status(400).json({ message: 'Por favor, verifique se passou o título da mensagem.' });
-    }
-    if (!description) {
-        return response.status(400).json({ message: 'Por favor, verifique se passou a descrição da mensagem.' });
-    }
+  if (!message) {
+    return response
+      .status(404)
+      .json({ message: "Por favor, informe um id válido da mensagem" });
+  }
+  if (!title) {
+    return response
+      .status(400)
+      .json({
+        message: "Por favor, verifique se passou o título da mensagem.",
+      });
+  }
+  if (!description) {
+    return response
+      .status(400)
+      .json({
+        message: "Por favor, verifique se passou a descrição da mensagem.",
+      });
+  }
 
-    message.title = title;
-    message.description = description;
+  message.title = title;
+  message.description = description;
 
-    return response.status(200).json({ message: 'Mensagem atualizada com sucesso!', updatedMessage: message });
+  return response
+    .status(200)
+    .json({
+      message: "Mensagem atualizada com sucesso!",
+      updatedMessage: message,
+    });
 });
 // Endpoint para deletar as mensagens
-app.delete('/message/:id', (request, response) => {
-    const { id } = request.params;
+app.delete("/message/:id", (request, response) => {
+  const { id } = request.params;
 
-    const messageIndex = messages.findIndex(msg => msg.id === parseInt(id));
+  const messageIndex = messages.findIndex((msg) => msg.id === parseInt(id));
 
-    if (messageIndex === -1) {
-        return response.status(404).json({ message: 'Mensagem não encontrada, verifique o identificador em nosso banco' });
-    }
+  if (messageIndex === -1) {
+    return response
+      .status(404)
+      .json({
+        message:
+          "Mensagem não encontrada, verifique o identificador em nosso banco",
+      });
+  }
 
-    messages.splice(messageIndex, 1);
+  messages.splice(messageIndex, 1);
 
-    return response.status(200).json({ message: 'Mensagem apagada com sucesso!' });
+  return response
+    .status(200)
+    .json({ message: "Mensagem apagada com sucesso!" });
 });
+
+app.get("/details/:id", (request, response) => {
+  const { id } = request.params
+
+  const note = messages.find(message => message.id == id)
+
+  if (!note) {
+    return response.status(404).json({
+      message: "Recado não encontrado."
+    })
+  }
+
+  response.status(200).json(note)
+})
+
+
+
+
+
 
 // Endpoint inicial
 app.get("/", (request, response) => {
